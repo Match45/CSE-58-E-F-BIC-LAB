e8fa0af3e9f7c9b9177571a952f06ce0444b88d4
Mahmooda Choudhury <150023990+Match45@users.noreply.github.com>
2026-06-16 16:32:15 +0600

    solution problem 2
    Implement pattern matching with mismatches up to d.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,19 @@
+pattern = input().strip()
+text = input().strip()
+d = int(input())
+positions=[]
 
+for i in range(len(text)-len(pattern)+1):
+    mismatch=0
+    
+    for j in range(len(pattern)):
+        if pattern[j] != text[i+j]:
+            mismatch+=1
+            
+        if mismatch>d:
+            break
+        
+    if mismatch<=d:
+        positions.append(str(i))
+        
+print(" ".join(positions))
